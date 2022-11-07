5e44903f003c9d71cf30a92f74e5ffeeeedf5d9c
Jakub Jóźwiak <59959607+derwis13@users.noreply.github.com>
2022-11-07 01:09:18 +0100

    add map app

--- Kotlin/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ Kotlin/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
@@ -98,9 +100,14 @@ class GameFragment : Fragment() {
                         setQuestion()
                         binding.invalidateAll()
                     } else {
-                        // We've won!  Navigate to the gameWonFragment.
+                        view.findNavController()
+                            .navigate(GameFragmentDirections
+                                .actionGameFragmentToGameWonFragment(numQuestions, questionIndex))// We've won!  Navigate to the gameWonFragment.
                     }
                 } else {
+                    view.findNavController()
+                        .navigate(GameFragmentDirections
+                            .actionGameFragmentToGameOverFragment())
                     // Game over! A wrong answer sends us to the gameOverFragment.
                 }
             }
